85e84bafe1983420c2530e5be198f849af4b104d
Alex Chaffee <alex@stinky.com>
2014-06-01 13:22:56 -0400

    remove rogue print statement
    this `puts` was cluttering our logs

--- lib/geo_point.rb
+++ lib/geo_point.rb
@@ -128,8 +128,6 @@ class GeoPoint
 
   def to_coords points
     meth = :"to_#{coord_mode}"
-    puts "to_coords #{points.inspect} - #{coord_mode} - #{points.class}"
-    # return points.send(meth) if points.first.kind_of? String
     points.send(meth)
   end
   
